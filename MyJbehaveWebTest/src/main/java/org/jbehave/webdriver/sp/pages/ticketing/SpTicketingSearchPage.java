77a5a1379620896a83b1b8e1d98a19ad3e98d134
javaguy0726 <terai2012@gmail.com>
2014-01-23 18:38:34 +0800

    开始做其他页面的case

--- MyJbehaveWebTest/src/main/java/org/jbehave/webdriver/sp/pages/ticketing/SpTicketingSearchPage.java
+++ MyJbehaveWebTest/src/main/java/org/jbehave/webdriver/sp/pages/ticketing/SpTicketingSearchPage.java
@@ -14,8 +14,37 @@ public class SpTicketingSearchPage extends SpAbstractPage {
 	public static By sp_ticketing_searchpage_link_search;
 	public static By sp_ticketing_searchpage_link_clear;
 	public static By sp_ticketing_searchpage_span_msg;
-	public static By sp_ticketing_searchpage_input_incidentid;
-	public static By sp_ticketing_searchpage_;
+	public static By sp_ticketing_searchpage_label_incidentno;
+	public static By sp_ticketing_searchpage_label_description;
+	public static By sp_ticketing_searchpage_label_priority;
+	public static By sp_ticketing_searchpage_label_customer_ticketno;
+	public static By sp_ticketing_searchpage_label_status;
+	public static By sp_ticketing_searchpage_label_status_open;
+	public static By sp_ticketing_searchpage_label_status_assigned;
+	public static By sp_ticketing_searchpage_label_status_inprogress;
+	public static By sp_ticketing_searchpage_label_status_schedule;
+	public static By sp_ticketing_searchpage_label_status_csg;
+	public static By sp_ticketing_searchpage_label_status_hold;
+	public static By sp_ticketing_searchpage_label_status_resolved;
+	public static By sp_ticketing_searchpage_label_status_closed;
+	public static By sp_ticketing_searchpage_label_noofdays;
+	public static By sp_ticketing_searchpage_label_noofdays_na;
+	public static By sp_ticketing_searchpage_label_noofdays_today;
+	public static By sp_ticketing_searchpage_label_noofdays_1;
+	public static By sp_ticketing_searchpage_label_noofdays_30;
+	public static By sp_ticketing_searchpage_label_noofdays_60;
+	public static By sp_ticketing_searchpage_label_noofdays_90;
+	public static By sp_ticketing_searchpage_label_customer;
+	public static By sp_ticketing_searchpage_label_shiptoaddress;
+	public static By sp_ticketing_searchpage_label_incidentdate;
+	public static By sp_ticketing_searchpage_label_closedate;
+	public static By sp_ticketing_searchpage_label_contact;
+	public static By sp_ticketing_searchpage_label_requester;
+	public static By sp_ticketing_searchpage_label_unitid;
+	public static By sp_ticketing_searchpage_label_feowner;
+	public static By sp_ticketing_searchpage_label_contract;
+	public static By sp_ticketing_searchpage_label_sro;
+	public static By sp_ticketing_searchpage_label_srostatus;
 	
 	
 	
