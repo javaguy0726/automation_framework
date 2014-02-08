0443ed994b6d09c19f7d8a27551c4e929915b42b
javaguy0726 <terai2012@gmail.com>
2014-02-08 12:28:00 +0800

    找到了包找不到资源文件异常的原因

--- MyJbehaveWebTest/src/main/java/org/jbehave/webdriver/sp/stories/ticketing/TicketingSearch.java
+++ MyJbehaveWebTest/src/main/java/org/jbehave/webdriver/sp/stories/ticketing/TicketingSearch.java
@@ -1,11 +1,17 @@
 package org.jbehave.webdriver.sp.stories.ticketing;
 
+import static java.util.Arrays.asList;
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+
+import java.util.List;
+
 import org.jbehave.core.annotations.UsingSteps;
+import org.jbehave.core.io.StoryFinder;
 import org.jbehave.webdriver.steps.LoginSteps;
 import org.jbehave.webdriver.steps.TicketingSearchSteps;
 import org.jbehave.webdriver.stories.BaseStories;
 
 @UsingSteps(instances = {LoginSteps.class,TicketingSearchSteps.class})
 public class TicketingSearch extends BaseStories {
-
+	
 }
